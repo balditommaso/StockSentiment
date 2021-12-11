1f32ad5bcdee98b9e726c9f1dfbb233a419a631f
edoardoruffoli <ruffoli99@gmail.com>
2021-12-11 14:49:58 +0100

    Added training_sentiment.py

--- training/training_sentiment.py
+++ training/training_sentiment.py
@@ -1,11 +1,17 @@
+import joblib
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import metrics
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+
 from sklearn.pipeline import Pipeline
 import re
 from langdetect import detect
@@ -56,7 +62,7 @@ def select_only_english(df):
     for index, row in df.iterrows():
 
         try:
-            if detect(row['Text']) != 'en':
+            if detect(row['text']) != 'en':
                 df.drop(index, inplace=True)
                 print(f'Line {index} dropped')
         except:
@@ -66,47 +72,50 @@ def select_only_english(df):
     return df
 
 def train():
-    df = pd.read_json('tsla.json', lines=True)
-    df = df['Text']
-    df.to_csv('tsla.csv', index=False)
-    """
-    #    df = df.sample(frac=0.02)
-    df['Text'] = df['Text'].str.lower()
-    df['Text'] = df['Text'].apply(removeSpecialChars)
-    df['Text'] = df['Text'].apply(removeAllNonAlpha)
-    df = select_only_english(df)
-    
-    
-    """
+    df = pd.read_csv('news_with_sentiment.csv')
 
-    training_set, test_set = train_test_split(df, test_size=0.2)
+    #Code for data preprocessing
+    #    df = df.sample(frac=0.02) to use when the dataset is too large
+    #    #df = df['text']
+    df['text'] = df['text'].str.lower()
+    df['text'] = df['text'].apply(removeSpecialChars)
+    df['text'] = df['text'].apply(removeAllNonAlpha)
+    #df = select_only_english(df)
 
-    print(len(training_set))
+    #df.to_csv('training_set_to_label.csv', index=False)
+
+
+    training_set, test_set = train_test_split(df, test_size=0.2, random_state=11)
+
 
     # Pipeline Classifier
     clf = Pipeline([
         ('vect', CountVectorizer()),
         ('tfidf', TfidfTransformer()),
-        ('clf', MultinomialNB()),
+        ('clf', XGBClassifier()),   # XGBClassifier è il migliore in acc ma basso positive recall
+                                    # Random Forest Classifier, Logistic Regression e SVC sono buoni (0.75 acc)
+                                    # MultinomialNB e BernoulliNB meno buoni (0.67 acc)
     ])
 
     # Training the Pipeline Classifier
-    clf.fit(training_set.text, training_set.label)
+    clf.fit(training_set.text, training_set.target)
 
     #Testing of the Pipeline
-    docs_test = test_set['text']
-    print(docs_test)
-    predicted = clf.predict(docs_test)
+    predicted = clf.predict(test_set['text'])
 
     #Extracting statistics and metrics
-    accuracy = accuracy_score(predicted, test_set['label'])
+    accuracy = accuracy_score(predicted, test_set['target'])
     print("Accuracy on test set: ", accuracy)
     print("Metrics per class on test set:")
 
     print("Confusion matrix:")
-    metrics.confusion_matrix(test_set['label'], predicted)
+    metrics.confusion_matrix(test_set['target'], predicted)
 
-    print(metrics.classification_report(test_set.label, predicted,
-        target_names=["0","1", "2"]))
+    print(metrics.classification_report(test_set['target'], predicted,
+        target_names=["positive", "neutral", "negative"]))
+
+    # Save the classifier
+    #joblib.dump(clf, 'sentiment_classifier.pkl')
+
 
 train()
